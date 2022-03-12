982099a13bf6b1c0dbdf7017729d7fbb30dd82f3
emkael <emkael@tlen.pl>
2022-03-12 21:08:06 +0100

    Parameter to force (re)generation of the brackets

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -2,9 +2,11 @@
 
 export GIT_SSH_COMMAND="ssh ${LIGA_SSH_OPTS}"
 
+FORCE=${1:-0}
+
 PREVHEAD=$(git rev-parse HEAD)
 git pull --quiet --rebase --autostash --recurse-submodules --no-stat
-if [ -n "$(git diff --name-status --no-renames $PREVHEAD HEAD)" ]
+if [ -n "$(git diff --name-status --no-renames $PREVHEAD HEAD)" -o $FORCE != "0" ]
 then
     ( date --rfc-3339=seconds; ./generate.sh; ./sync.sh; date --rfc-3339=seconds; ) > ${LIGA_PLAYOFF_LOGFILE} 2>&1
 fi
